7a8c68ae9e78b09e807246222149a691791edbe6
Tomas Rodriguez <mazzito2015@gmail.com>
2022-02-19 14:55:22 -0500

    Definiendo el modo development en el webpack.config.js para evitar warnings y que todo trabaje ok

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require ('html-webpack-plugin');
 
 module.exports = {
-    entry: '.src/index.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',        
-    }, 
+    },
+    mode: 'development' ,
     resolve: {
         extensions: ['.js', '.jsx'],
     },
