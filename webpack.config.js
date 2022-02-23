df840b429182ee65e5488ea8f47245a322b5b451
Tomas Rodriguez <mazzito2015@gmail.com>
2022-02-22 21:30:52 -0500

    Estado del proyecto antes de subirlo a la nube

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require ('html-webpack-plugin');
+const MiniCssExtractPlugin = require ('mini-css-extract-plugin');
+
 
 module.exports = {
     entry: './src/index.js',
@@ -28,6 +30,14 @@ module.exports = {
                         loader: 'html-loader'
                     }
                 ]
+            }, 
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
             }
         ]     
     },
@@ -35,6 +45,11 @@ module.exports = {
          new HtmlWebpackPlugin ({
              template:'./public/index.html',
              filename: './index.html'
-         }),   
-    ]
+         }),
+         new MiniCssExtractPlugin(
+             {
+                 filename: '[name].css'
+             }
+         )   
+    ],    
 }
